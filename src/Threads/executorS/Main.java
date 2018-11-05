92be2b8e9d2bda673c5c0fcdf717262e3f5aa0f6
LektorZX <lektor_in@mail.ru>
2018-11-05 21:49:33 +0300

    2 comm

--- src/Threads/executorS/Main.java
+++ src/Threads/executorS/Main.java
@@ -3,14 +3,21 @@ package Threads.executorS;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
         CountDownLatch countDownLatch = new CountDownLatch(5);
         ExecutorService executorService= Executors.newFixedThreadPool(2);//пул на два потока
         executorService.execute(new SH(countDownLatch));//принимает только ранебл,и потом уже создает поток
-executorService.execute(new Rosket(countDownLatch));
+        executorService.execute(new Rosket(countDownLatch));
+        executorService.shutdown();
+        try {
+            executorService.awaitTermination(1L, TimeUnit.HOURS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+    }
 
 
         public static class Rosket implements Runnable{
@@ -43,12 +50,12 @@ executorService.execute(new Rosket(countDownLatch));
             public void run() {
                 for (int i = 5; i >0 ; i--) {
                     try {
-                        Thread.sleep(100L);
+                        Thread.sleep(1000L);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                     countDownLatch.countDown();
-                    System.out.println("-1");
+                    System.out.println(i);
 
                 }
 
